95c51223861964dd603589bebd0413d76fd24f8c
4mga-tech <gansukhamgalanbaatarr133@gmail.com>
2025-10-01 17:11:24 +0800

    lol

--- chess/src/pages/login.js
+++ chess/src/pages/login.js
@@ -19,7 +19,7 @@ function Login() {
 
         <form onSubmit={handleSubmit} className="space-y-5">
           <div>
-            <label className="block text-gray-300 mb-1">Имэйл</label>
+            <label className="block text-gray-300 mb-1">Имэйлl</label>
             <input
               type="email"
               className="w-full px-4 py-2 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
